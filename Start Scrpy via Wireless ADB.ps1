a09de17b361c39ee0d510ccddc5edeb030d6d408
Blavkentropy1 <76550896+Blavkentropy1@users.noreply.github.com>
2022-10-24 23:41:36 +0000

    Update Start Scrpy via Wireless ADB.ps1

--- Start Scrpy via Wireless ADB.ps1
+++ Start Scrpy via Wireless ADB.ps1
@@ -14,22 +14,22 @@ $Port = Read-Host -Prompt "Connect Port"
 
 cd $Scrpy_Location
 
-Write-Output -InputObject (adb connect ${Phone_IP}:${port} ) -OutVariable Output
+Write-Output -InputObject (.\adb.exe connect ${Phone_IP}:${port} ) -OutVariable Output
  
 if ($Output -match "failed" -or $Output -match "no host") 
         {
         Write-Output -InputObject "Unable to connect, attempting to Pair Phone"
         new-variable -name Pair_Port -value (Read-Host -Prompt "Pair Port")
         new-variable -name Pair_Code -value (Read-Host -Prompt "Pair Code")
-        adb pair ${Phone_IP}:${Pair_Port} $Pair_Code 
+        .\adb.exe pair ${Phone_IP}:${Pair_Port} $Pair_Code 
         Write-Output -InputObject "Succesfully Paired, Connecting ADB"
-        adb connect ${Phone_IP}:${port}
+        .\adb.exe connect ${Phone_IP}:${port}
         Write-Output -InputObject "Succesfully Connected, starting Scrcpy"
-        scrcpy.exe --turn-screen-off
+        .\scrcpy.exe --turn-screen-off
         }
 Else {
        Write-Output -InputObject "Succesfully Connected, starting Scrcpy"
-       scrcpy.exe --turn-screen-off
+       .\scrcpy.exe --turn-screen-off
        }
        
-adb disconnect ${Phone_IP}:${port}
+.\adb.exe disconnect ${Phone_IP}:${port}
