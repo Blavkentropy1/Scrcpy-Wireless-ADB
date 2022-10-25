c1f876c8e556583fc59b0f32c89ea72ee0baafe4
Blavkentropy1 <76550896+Blavkentropy1@users.noreply.github.com>
2022-10-25 04:28:25 +0000

    Update Start Scrpy via Wireless ADB.ps1

--- Start Scrpy via Wireless ADB.ps1
+++ Start Scrpy via Wireless ADB.ps1
@@ -12,18 +12,18 @@ $Phone_IP = "192.168.0.xxx"                        #------------Needs a IP
 $Port = Read-Host -Prompt "Connect Port"
 #=========================[  END:Parameters   ]============================
 
-.\adb.exe disconnect #<---------- Used to clear all Previous Sessions, Can be Removed
+.\adb.exe disconnect
 cd $Scrpy_Location
+
 Write-Output -InputObject (.\adb.exe connect ${Phone_IP}:${port} ) -OutVariable Output
 
 if ($Output -match "connected to") 
         {
         Write-Output -InputObject "Succesfully Connected, starting Scrcpy"
-        .\scrcpy.exe --turn-screen-off
+        .\scrcpy.exe --tcpip=${Phone_IP}:${Port} --turn-screen-off
         Write-Output -InputObject "Scrcpy Has been Closed. Exiting"
         exit
         }
-     
 if ($Output -match "failed" -or $Output -match "no host") 
         {
         Write-Output -InputObject "Unable to connect, attempting to Pair Phone"
@@ -31,17 +31,17 @@ if ($Output -match "failed" -or $Output -match "no host")
         new-variable -name Pair_Port -force -value (Read-Host -Prompt "Pair Port")
         Write-Output -InputObject ( .\adb.exe pair ${Phone_IP}:${Pair_Port} $Pair_Code ) -OutVariable Output_Pair
 
-        if ($Output_pair -match "Failed: Wrong password or connection was dropped." -or $Output_pair -match "Failed to parse address for pairing" -or $output_pair -match "failed to connect to") 
+  if ($Output_pair -match "Failed: Wrong password or connection was dropped." -or $Output_pair -match "Failed to parse address for pairing" -or $output_pair -match "failed to connect to") 
                 {
                 Write-Output -InputObject "Wrong Pair Pin, or Pair Port"
                 Pause
                 return
                 }              
-        Else    {
+   Else    {
                 Write-Output -InputObject "Succesfully Paired, Connecting ADB"
                 .\adb.exe connect ${Phone_IP}:${port}
                 Write-Output -InputObject "Succesfully Connected, starting Scrcpy"
-                .\scrcpy.exe --turn-screen-off
+                .\scrcpy.exe --tcpip=${Phone_IP}:${Port} --turn-screen-off
                 Write-Output -InputObject "Scrcpy Has been Closed. Exiting"
                 exit
             }
