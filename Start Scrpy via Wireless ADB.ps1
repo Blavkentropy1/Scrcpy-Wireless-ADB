948915352c43968f3ff472a9c0804b984b8f2b51
Blavkentropy1 <76550896+Blavkentropy1@users.noreply.github.com>
2022-10-25 02:09:03 +0000

    Update Start Scrpy via Wireless ADB.ps1

--- Start Scrpy via Wireless ADB.ps1
+++ Start Scrpy via Wireless ADB.ps1
@@ -27,22 +27,36 @@ if ($Output -match "failed" -or $Output -match "no host")
         Write-Output -InputObject "Unable to connect, attempting to Pair Phone"
         new-variable -name Pair_Code -force -value (Read-Host -Prompt "Pair Code")
         new-variable -name Pair_Port -force -value (Read-Host -Prompt "Pair Port")
-        .\adb.exe pair ${Phone_IP}:${Pair_Port} $Pair_Code 
-        Write-Output -InputObject "Succesfully Paired, Connecting ADB"
-        .\adb.exe connect ${Phone_IP}:${port}
-        Write-Output -InputObject "Succesfully Connected, starting Scrcpy"
-        .\scrcpy.exe --turn-screen-off
-        Write-Output -InputObject "Scrcpy Has been Closed. Exiting"
-        exit
+
+        Write-Output -InputObject ( .\adb.exe pair ${Phone_IP}:${Pair_Port} $Pair_Code ) -OutVariable Output_Pair
+
+        if ($Output_pair -match "Failed: Wrong password or connection was dropped." -or $Output_pair -match "Failed to parse address for pairing" -or $output_pair -match "failed to connect to") 
+                {
+                Write-Output -InputObject "Wrong Pair Pin, or Pair Port"
+                Pause
+                return
+                }              
+        Else    {
+                Write-Output -InputObject "Succesfully Paired, Connecting ADB"
+                .\adb.exe connect ${Phone_IP}:${port}
+                Write-Output -InputObject "Succesfully Connected, starting Scrcpy"
+                .\scrcpy.exe --turn-screen-off
+                Write-Output -InputObject "Scrcpy Has been Closed. Exiting"
+                exit
+            }
+
         }
-if ($Output -match "cannot connect to") 
+
+if ($Output -match "cannot connect to" -or $Output -match "bad port number") 
         {
-        Write-Output -InputObject "Port appears to be Incorrect Exiting"
+        Write-Output -InputObject "Port appears to be Incorrect - Exiting"
         Pause
         exit
         }
+
+
 Else {
-       Write-Output -InputObject "Unknown Output, Check error Exiting"
+       Write-Output -InputObject "Unknown Output, Check error - Exiting"
        Pause
        exit
        }
