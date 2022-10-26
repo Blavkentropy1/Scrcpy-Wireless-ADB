779e6b2f5f21d159a86e48f9ffa234272d9cc242
Blavkentropy1 <76550896+Blavkentropy1@users.noreply.github.com>
2022-10-26 07:46:27 +0000

    Update Start Scrpy via Wireless ADB.ps1

--- Start Scrpy via Wireless ADB.ps1
+++ Start Scrpy via Wireless ADB.ps1
@@ -3,22 +3,19 @@
     Description   : Wirelessly connect ADB to device, and Pair ADB if required #>
     
 Write-Output -InputObject "Start Scrcpy via Wireless ADB"
-
 #=========================[ BEGIN:Parameters  ]============================
 $Scrpy_Location = "D:\Scrcpy\scrcpy-win64-v1.24"
-$Phone_IP = "192.168.0.101"
-$Arguments = "-Sw --power-off-on-close"
+$Phone_IP = "192.168.0.xxx"
+$Arg0 = "-Sw"
+$Arg1 = "--power-off-on-close"
 #=========================[  END:Parameters   ]============================
-
 cd $Scrpy_Location
 
 Do  {
 #new-variable -name Phone_IP -force -value (Read-Host -Prompt "Phone IP")
 new-variable -name Port -force -value (Read-Host -Prompt "Connect Port")
-
 Write-Output -InputObject (.\adb.exe connect ${Phone_IP}:${port} ) -OutVariable Output
-
-if ($Output -match "failed" -or $Output -match "no host") 
+    if ($Output -match "failed" -or $Output -match "no host") 
         {
             Write-Output -InputObject "Unable to connect, attempting to Pair Phone"
             do
@@ -26,23 +23,20 @@ if ($Output -match "failed" -or $Output -match "no host")
                 new-variable -name Pair_Code -force -value (Read-Host -Prompt "Wifi Pairing code")
                 new-variable -name Pair_Port -force -value (Read-Host -Prompt "Pair Port")
                 Write-Output -InputObject ( .\adb.exe pair ${Phone_IP}:${Pair_Port} $Pair_Code ) -OutVariable Output_Pair
-
-                if ($Output_pair -match "Failed: Wrong password or connection was dropped." -or $Output_pair -match "Failed to parse address for pairing" -or $output_pair -match "failed to connect to" -or $Output_pair -match "Failed: Unable to start pairing client.") 
+                    if ($Output_pair -match "Failed: Wrong password or connection was dropped." -or $Output_pair -match "Failed to parse address for pairing" -or $output_pair -match "failed to connect to" -or $Output_pair -match "Failed: Unable to start pairing client.") 
                     {
                        Write-Output -InputObject "Wrong Wifi Pairing code, or Pair Port"                    
                     }
             }
-      
             Until ($Output_pair -match "Successfully paired to")
                     Write-Output -InputObject "Device Paired"   
                     Write-Output -InputObject (.\adb.exe connect ${Phone_IP}:${Port} ) -OutVariable Output      
          }
-       
     if ($Output -match "10061" -or $Output -match "bad port number") 
-           {
-           Write-Output -InputObject "Incorrect Port Input"
-           }
-    }
-     
+        {
+        Write-Output -InputObject "Incorrect Port Input"
+        }
+    }   
 Until($Output -match "connected to")
-Start-Process -FilePath .\scrcpy.exe -ArgumentList "--tcpip=${Phone_IP}:${Port} ${Arguments}"
+.\scrcpy.exe --tcpip=${Phone_IP}:${Port} ${Arg0} ${Arg1}
+.\adb.exe disconnect ${Phone_IP}:${Port}
